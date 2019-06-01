21342e9ac3bea3c3d21444a59f55915562a08e8c
MadEwink <adrien.floriant@telecom-sudparis.eu>
2019-06-01 18:24:02 +0200

    imported project in clion, added gitignore, removed main from Law\ Enforcer.cpp

---
+++ Law Enforcer/Law Enforcer/LevelObject.h
@@ -0,0 +1,14 @@
+//
+// Created by mad on 01/06/19.
+//
+
+#ifndef LAW_ENFORCER_LEVELOBJECT_H
+#define LAW_ENFORCER_LEVELOBJECT_H
+
+
+class LevelObject {
+
+};
+
+
+#endif //LAW_ENFORCER_LEVELOBJECT_H
